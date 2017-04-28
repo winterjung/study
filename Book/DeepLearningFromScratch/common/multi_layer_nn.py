943fac84c70964c26647343a8091a7f549056fbe
winterJ <wintermy201@naver.com>
2017-04-28 14:50:21 +0900

    Separate general neural network class from ipynb

---
+++ Book/DeepLearningFromScratch/common/multi_layer_nn.py
@@ -0,0 +1,102 @@
+import numpy as np
+from collections import OrderedDict
+from .layers import Relu, Sigmoid, SoftmaxWithLoss, Affine
+
+
+class MultiLayerNet:
+    """완전연결 다층 신경망
+
+    Parameters
+    ----------
+    input_size : 입력 크기（MNIST의 경우엔 784）
+    hidden_size_list : 각 은닉층의 뉴런 수를 담은 리스트（e.g. [100, 100, 100]）
+    output_size : 출력 크기（MNIST의 경우엔 10）
+    activation : 활성화 함수 - 'relu', 'sigmoid' 혹은 커스텀함수
+    weight_init_std : 가중치의 표준편차 지정（e.g. 0.01)
+    """
+    def __init__(self, input_size, hidden_size_list, output_size,
+                 activation='relu', weight_init_std=0.01):
+        self.input_size = input_size
+        self.output_size = output_size
+        self.hidden_size_list = hidden_size_list
+        self.hidden_layer_num = len(hidden_size_list)
+        self.params = {}
+
+        # 가중치 초기화
+        self.__init_weight(weight_init_std)
+
+        # 계층 생성
+        if activation in ['sigmoid', 'relu']:
+            activation_layer = {'sigmoid': Sigmoid, 'relu': Relu}
+        else:
+            # 직접 커스텀 함수로 전달한다면
+            active_func = activation
+            active_func_name = "custom"
+            activation_layer = {active_func_name: active_func}
+            activation = active_func_name
+
+        # 레이어 연결
+        self.layers = OrderedDict()
+        for idx in range(1, self.hidden_layer_num+1):
+            self.layers['Affine' + str(idx)] = Affine(self.params['W' + str(idx)],
+                                                      self.params['b' + str(idx)])
+            self.layers['Activation_function' + str(idx)] = activation_layer[activation]()
+
+        idx = self.hidden_layer_num + 1
+        self.layers['Affine' + str(idx)] = Affine(self.params['W' + str(idx)], self.params['b' + str(idx)])
+
+        self.last_layer = SoftmaxWithLoss()
+
+    def __init_weight(self, weight_init_std):
+        # 가중치 초기화
+        all_size_list = [self.input_size] + self.hidden_size_list + [self.output_size]
+        for idx in range(1, len(all_size_list)):
+            scale = weight_init_std
+
+            # 뒤에서 배울 가중치 초기화
+            # 이부분이 없으면 학습이 진행되지 않음
+            if str(weight_init_std).lower() in ('relu', 'he'):
+                scale = np.sqrt(2.0 / all_size_list[idx - 1])  # ReLU를 사용할 때의 권장 초깃값
+            elif str(weight_init_std).lower() in ('sigmoid', 'xavier'):
+                scale = np.sqrt(1.0 / all_size_list[idx - 1])  # sigmoid를 사용할 때의 권장 초깃값
+
+            self.params['W' + str(idx)] = scale * np.random.randn(all_size_list[idx-1], all_size_list[idx])
+            self.params['b' + str(idx)] = np.zeros(all_size_list[idx])
+
+    def predict(self, x):
+        for layer in self.layers.values():
+            x = layer.forward(x)
+
+        return x
+
+    def loss(self, x, t):
+        y = self.predict(x)
+        return self.last_layer.forward(y, t)
+
+    def gradient(self, x, t):
+        """
+        Returns
+        -------
+        각 층의 기울기를 담은 딕셔너리(dictionary) 변수
+            grads['W1']、grads['W2']、... 각 층의 가중치
+            grads['b1']、grads['b2']、... 각 층의 편향
+        """
+        # forward
+        self.loss(x, t)
+
+        # backward
+        dout = 1
+        dout = self.last_layer.backward(dout)
+
+        layers = list(self.layers.values())
+        layers.reverse()
+        for layer in layers:
+            dout = layer.backward(dout)
+
+        # 결과 저장
+        grads = {}
+        for idx in range(1, self.hidden_layer_num+2):
+            grads['W' + str(idx)] = self.layers['Affine' + str(idx)].dW
+            grads['b' + str(idx)] = self.layers['Affine' + str(idx)].db
+
+        return grads
