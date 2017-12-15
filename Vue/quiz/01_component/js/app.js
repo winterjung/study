d438b2910773ebb21c349a3eac64c26c03713d86
JungWinter <wintermy201@gmail.com>
2017-12-15 18:08:44 +0900

    Solve quiz 01
    Signed-off-by: JungWinter <wintermy201@gmail.com>

--- Vue/quiz/01_component/js/app.js
+++ Vue/quiz/01_component/js/app.js
@@ -5,6 +5,10 @@ Vue.component('todo-item', {
 
 // 실습 #1 - `todo-footer` 컴포넌트 전역 등록
 // <p>This is another child global component</p> 를 template 으로 갖는 컴포넌트를 등록해보세요.
+Vue.component('todo-footer', {
+    template: '<p>This is another child global component</p>'
+})
+
 
 var app = new Vue({
     el: '#app',
@@ -14,4 +18,10 @@ var app = new Vue({
 
     // 실습 #2 - `todo-footer` 컴포넌트 지역 등록
     // <p>This is another child local component</p> 를 template 으로 갖는 컴포넌트를 등록해보세요.
+    components: {
+        // component 변수를 생성해서 할당해도 됨
+        'todo-footer': {
+            template: '<p>This is another child local component</p>'
+        }
+    }
 });
