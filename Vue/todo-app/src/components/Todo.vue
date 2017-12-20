416bd2c80b35c141ab69ab5beb358d6d25c4fbf8
jungwinter <wintermy201@gmail.com>
2017-12-20 18:25:48 +0900

    Separate template of TodoList into Todo.vue
    Signed-off-by: jungwinter <wintermy201@gmail.com>

---
+++ Vue/todo-app/src/components/Todo.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="ui centered card">
+    <div class="content">
+      <div class="header">
+        {{ todo.title }}
+      </div>
+      <div class="description">
+        {{ todo.description }}
+      </div>
+      <div class="extra content">
+        <span class="right floated edit icon">
+          <i class="edit icon"></i>
+        </span>
+      </div>
+    </div>
+    <div class="ui bottom attached green basic button" v-show="todo.done">
+      완료됨
+    </div>
+    <div class="ui bottom attached red basic button" v-show="!todo.done">
+      완료
+    </div>
+  </div>
+</template>
+
+<script type="text/javascript">
+export default {
+  props: ['todo'],
+};
+</script>
--- Vue/todo-app/src/components/TodoList.vue
+++ Vue/todo-app/src/components/TodoList.vue
@@ -3,33 +3,18 @@
     <p>완료된 작업 수: {{ childTodos.filter(todo => { return todo.done === true }).length }}</p>
     <p>진행중인 작업 수: {{ childTodos.filter(todo => { return todo.done === false }).length }}</p>
 
-    <div class="ui centered card" v-for="todo in childTodos" :key="todo.title">
-      <div class="content">
-        <div class="header">
-          {{ todo.title }}
-        </div>
-        <div class="description">
-          {{ todo.description }}
-        </div>
-        <div class="extra content">
-          <span class="right floated edit icon">
-            <i class="edit icon"></i>
-          </span>
-        </div>
-      </div>
-      <div class="ui bottom attached green basic button" v-show="todo.done">
-        완료됨
-      </div>
-      <div class="ui bottom attached red basic button" v-show="!todo.done">
-        완료
-      </div>
-    </div>
+    <my-todo v-for="todo in childTodos" v-bind:todo="todo" :key="todo.title"></my-todo>
   </div>
 </template>
 
 <script type="text/javascript">
+import myTodo from './Todo';
+
 export default {
   props: ['childTodos'],
+  components: {
+    myTodo,
+  },
 };
 </script>
 
