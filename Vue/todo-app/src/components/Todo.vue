39842dea36ad43d81992da779a53b91424257aec
jungwinter <wintermy201@gmail.com>
2017-12-20 18:48:10 +0900

    Implement edit mode in Todo
    Signed-off-by: jungwinter <wintermy201@gmail.com>

--- Vue/todo-app/src/components/Todo.vue
+++ Vue/todo-app/src/components/Todo.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="ui centered card">
-    <div class="content">
+    <div class="content" v-show="!isEditing">
       <div class="header">
         {{ todo.title }}
       </div>
@@ -8,16 +8,34 @@
         {{ todo.description }}
       </div>
       <div class="extra content">
-        <span class="right floated edit icon">
+        <span class="right floated edit icon" v-on:click="showForm">
           <i class="edit icon"></i>
         </span>
       </div>
     </div>
-    <div class="ui bottom attached green basic button" v-show="todo.done">
+    <!-- 수정 모드에서 보이게 될 컨텐츠 -->
+    <div class="content" v-show="isEditing">
+      <div class="ui form">
+        <div class="field">
+          <label>제목</label>
+          <input type="text" v-model="todo.title">
+        </div>
+        <div class="field">
+          <label>설명</label>
+          <input type="text" v-model="todo.description">
+        </div>
+        <div class="ui two button attached buttons">
+          <button class="ui blue basic button" v-on:click="hideForm">
+            닫기
+          </button>
+        </div>
+      </div>
+    </div>
+    <div class="ui bottom attached green basic button" v-show="!isEditing && todo.done">
       완료됨
     </div>
-    <div class="ui bottom attached red basic button" v-show="!todo.done">
-      완료
+    <div class="ui bottom attached red basic button" v-show="!isEditing && !todo.done">
+      진행중
     </div>
   </div>
 </template>
@@ -25,5 +43,18 @@
 <script type="text/javascript">
 export default {
   props: ['todo'],
+  data() {
+    return {
+      isEditing: false,
+    };
+  },
+  methods: {
+    showForm() {
+      this.isEditing = true;
+    },
+    hideForm() {
+      this.isEditing = false;
+    },
+  },
 };
 </script>
