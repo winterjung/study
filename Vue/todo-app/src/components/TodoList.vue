1b060e3475767a3a826a2665fe451ba4563e4f4f
jungwinter <wintermy201@gmail.com>
2017-12-20 18:07:05 +0900

    Add basic structure of todo list
    Signed-off-by: jungwinter <wintermy201@gmail.com>

--- Vue/todo-app/src/components/TodoList.vue
+++ Vue/todo-app/src/components/TodoList.vue
@@ -1,16 +1,35 @@
 <template>
   <div>
-    <ul>
-      <li> Python 배우기 </li>
-      <li> Vue 배우기 </li>
-      <li> 1년 정리하기 </li>
-    </ul>
+    <p>완료된 작업 수: {{ childTodos.filter(todo => { return todo.done === true }).length }}</p>
+    <p>진행중인 작업 수: {{ childTodos.filter(todo => { return todo.done === false }).length }}</p>
+
+    <div v-for="todo in childTodos" :key="todo.title">
+      <div>
+        <div>
+          {{ todo.title }}
+        </div>
+        <div>
+          {{ todo.description }}
+        </div>
+        <div>
+          <span>
+            <i></i>
+          </span>
+        </div>
+      </div>
+      <div v-show="todo.done">
+        완료됨
+      </div>
+      <div v-show="!todo.done">
+        완료
+      </div>
+    </div>
   </div>
 </template>
 
 <script type="text/javascript">
 export default {
-  props: ['child-todos'],
+  props: ['childTodos'],
 };
 </script>
 
