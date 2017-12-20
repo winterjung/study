cb83741e626a53b0a1693e828be96c48fd9bb2d3
jungwinter <wintermy201@gmail.com>
2017-12-20 18:11:48 +0900

    Apply semantic-ui to todo list
    Signed-off-by: jungwinter <wintermy201@gmail.com>

--- Vue/todo-app/src/components/TodoList.vue
+++ Vue/todo-app/src/components/TodoList.vue
@@ -3,24 +3,24 @@
     <p>완료된 작업 수: {{ childTodos.filter(todo => { return todo.done === true }).length }}</p>
     <p>진행중인 작업 수: {{ childTodos.filter(todo => { return todo.done === false }).length }}</p>
 
-    <div v-for="todo in childTodos" :key="todo.title">
-      <div>
-        <div>
+    <div class="ui centered card" v-for="todo in childTodos" :key="todo.title">
+      <div class="content">
+        <div class="header">
           {{ todo.title }}
         </div>
-        <div>
+        <div class="description">
           {{ todo.description }}
         </div>
-        <div>
-          <span>
-            <i></i>
+        <div class="extra content">
+          <span class="right floated edit icon">
+            <i class="edit icon"></i>
           </span>
         </div>
       </div>
-      <div v-show="todo.done">
+      <div class="ui bottom attached green basic button" v-show="todo.done">
         완료됨
       </div>
-      <div v-show="!todo.done">
+      <div class="ui bottom attached red basic button" v-show="!todo.done">
         완료
       </div>
     </div>
