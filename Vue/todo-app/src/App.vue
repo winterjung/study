902f21004b435e1605275b366e887a4f494407f4
jungwinter <wintermy201@gmail.com>
2017-12-20 17:42:16 +0900

    Add data to component
    Signed-off-by: jungwinter <wintermy201@gmail.com>
    
    Use single quote on string

--- Vue/todo-app/src/App.vue
+++ Vue/todo-app/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <todo-list></todo-list>
+    <todo-list v-bind:child-todos="todos"></todo-list>
     <!-- 이것도 동작함 <TodoList></TodoList> -->
   </div>
 </template>
@@ -12,6 +12,34 @@ export default {
   name: 'app',
   components: {
     TodoList,
+  },
+  data() {
+    return {
+      todos: [
+        {
+          title: 'Python 배우기',
+          description: `Async를 이용한 비동기 작업 처리, 
+          소켓 통신 예제, pipenv를 사용한 프로젝트 구성,
+          CI적용, 파이썬 패턴 정리`,
+          done: false,
+        },
+        {
+          title: 'Vue 배우기',
+          description: 'Vue는 너무 어렵다. vue init만 했는데도 알지 못하는 파일이 무척 많이 생겼다.',
+          done: true,
+        },
+        {
+          title: '1년 정리하기',
+          description: '나는 1년동안 무엇을 했을까? 상반기엔 학교다니면서 학부연구생을 했다. 프로젝트는 했나?',
+          done: false,
+        },
+        {
+          title: '하반기 회고하기',
+          description: '소마도하고 발표도하고 블록체인 공부하고...',
+          done: false,
+        },
+      ],
+    };
   },
 };
 </script>
--- Vue/todo-app/src/components/TodoList.vue
+++ Vue/todo-app/src/components/TodoList.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
-      <ul>
-          <li> Python 배우기 </li>
-          <li> Vue 배우기 </li>
-          <li> 1년 정리하기 </li>
-      </ul>
+    <ul>
+      <li> Python 배우기 </li>
+      <li> Vue 배우기 </li>
+      <li> 1년 정리하기 </li>
+    </ul>
   </div>
 </template>
 
 <script type="text/javascript">
 export default {
-
+  props: ['child-todos'],
 };
 </script>
 
