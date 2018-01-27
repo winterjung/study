59dbb17e72928150b514976f028efab0b2bbdfff
jungwinter <wintermy201@gmail.com>
2018-01-27 15:21:06 +0900

    Main: use std::io to read input

--- Rust/guessing_game/src/main.rs
+++ Rust/guessing_game/src/main.rs
@@ -1,3 +1,11 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("숫자를 맞춰보세요!");
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+        .expect("실패!");
+
+    println!("너의 입력은: {}", guess);
 }
