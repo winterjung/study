eabc7b0f9729cd366e41c4a3724547255347ce1f
jungwinter <wintermy201@gmail.com>
2018-01-27 15:25:52 +0900

    Main: use rand to generate random number

--- Rust/guessing_game/src/main.rs
+++ Rust/guessing_game/src/main.rs
@@ -1,7 +1,14 @@
+extern crate rand;
+
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("숫자를 맞춰보세요!");
+    
+    let secret = rand::thread_rng().gen_range(1, 101);
+    println!("비밀 숫자는 {}", secret);
+    
     let mut guess = String::new();
 
     io::stdin().read_line(&mut guess)
